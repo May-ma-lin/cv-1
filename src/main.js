0f6e8da697e7b5b7811b22f846d7e32fe06a7f7b
midbezoar <2974490550@qq.com>
2022-05-14 11:02:48 +0800

    build

--- src/main.js
+++ src/main.js
@@ -1,19 +1,77 @@
 // querySelector: 匹配指定 CSS 选择器的一个元素
-let demo = document.querySelector('#demo');
-let string = `你好，我是一名前端新人`;
+let html = document.querySelector('#html');
+let style = document.querySelector('#style');
+let string = `/* 你好，我要做一个太极八卦
+ * 首先需要准备一个div
+ **/
+ #div1{
+  border: 1px solid red;
+  width: 200px;
+  height: 200px;
+ }
+ /* 接下来，把div变成八卦图
+  * 首先，把div变成一个圆
+ **/
+#div1{
+  border-radius: 50%;
+  box-shadow: 0 0 3px rgba(0,0,0,0.5);
+  border: none;
+}
+/* 八卦是阴阳形成的
+ * 一黑一白
+**/
+#div1{
+  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+  }
+/* 加两个神秘的小球 */
+#div1::before{
+  width: 100px;
+  height: 100px;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #000;
+  border-radius: 50%;
+  background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+  width: 100px;
+  height: 100px;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #fff;
+  border-radius: 50%;
+  background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+}
+`;
+
+let string2 = '';
 let n = 0;
 
-demo.innerHTML = string[n];
+// demo.innerHTML = string[n];
 
 let step = () => {
   setTimeout(() => {
-    // 如果你不清楚要 n+1 还是 n 或 n-1 的话，就一个个试
-    n = n + 1;
-    demo.innerHTML = string.substring(0, n);
-    if (n < string.length) {
+    // 如果发现是回车，就不照搬
+    // 发现不是就，照搬
+    if (string[n] === '\n') {
+      string2 += '<br>';
+    } else if (string[n] === ' ') {
+      string2 += '&nbsp;';
+    } else {
+      string2 += string[n];
+    }
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n);
+    window.scrollTo(0, 99999);
+    html.scrollTo(0, 99999);
+    if (n < string.length - 1) {
+      // 如果n不是最后一个，就继续
+      n += 1;
       step();
     }
-  }, 1000);
+  }, 0);
 };
 
-step();
+step(); // 1=>2
